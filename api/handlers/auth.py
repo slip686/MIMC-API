d19ee4dbb2238afdb19de2b823cac8bc9314f710
Kirill <slip686@gmail.com>
2023-11-03 16:21:06 +0300

    /auth/token handler fixed

--- api/handlers/auth.py
+++ api/handlers/auth.py
@@ -10,4 +10,6 @@ from config import Config
 def get_auth_token():
     user = multi_auth.current_user()
     token = user.generate_auth_token()
-    return {'api_token': token, 'stash_token': user.stash_token, 'email': user.email, 'user_id': user.user_id}, 200
+    return {'api_token': token, 'stash_token': user.stash_token, 'email': user.email, 'user_id': user.user_id,
+            'first_name': user.first_name, 'last_name': user.last_name, 'company_name': user.company_name,
+            'tin': user.tin}, 200
