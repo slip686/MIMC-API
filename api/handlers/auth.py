d96813de32a6f5940cb17f736272c8461c7ad70b
Kirill <slip686@gmail.com>
2023-11-03 16:53:50 +0300

    /auth/token handler fixed

--- api/handlers/auth.py
+++ api/handlers/auth.py
@@ -12,7 +12,7 @@ def get_auth_token():
     user = multi_auth.current_user()
     user_data = user_schema.dump(user)
     token = user.generate_auth_token()
-    return {'api_token': token, 'stash_token': user.stash_token, 'content': user_data}
+    return {'api_token': token, 'stash_token': user.stash_token} | user_data
     # return {'api_token': token, 'stash_token': user.stash_token, 'email': user.email, 'user_id': user.user_id,
     #         'first_name': user.first_name, 'last_name': user.last_name, 'company_name': user.company_name,
     #         'tin': user.tin, 'ntfcn_channel': user.ntfcn_channel}, 200
