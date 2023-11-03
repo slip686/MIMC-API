200a8402f7849d11667690fc66c6fbc403c4de25
Kirill <slip686@gmail.com>
2023-11-03 10:28:36 +0300

    wsgi.py fixed

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
-from api import app
+from api import create_app
 from api.handlers import auth, user, project, doc_place, docs, files, message
 
 
 if __name__ == '__main__':
-    app.run()
+    create_app().run()
